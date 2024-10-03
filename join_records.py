9e45f887441cbe6eb92ee8f8b8992ddbe639149d
Jacob Fry <30384236+spottedquoll@users.noreply.github.com>
2024-10-03 16:50:48 +0900

    Set country acronym in save directory to enable restarts

--- join_records.py
+++ join_records.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from config.config_reader import read_config
 from glob import glob
@@ -7,44 +8,69 @@ print('Join and filter COMTRADE records...')
 
 # Settings
 config = read_config()
-files = [f for f in glob(config.work_dir + "*.txt")]
-assert len(files) > 0
+filter_joined_by_hs6 = config.settings['filter_joined_by_hs6']
+filter_joined_by_transport_mode = config.settings['filter_joined_by_transport_mode']
 
-# Extract each file 
-print('Extracting ' + str(len(files)) + ' files...')
+# Save path
+save_dir = config.work_dir + str(config.year) + '/'
+assert os.path.isdir(save_dir)
 
-store = pd.DataFrame() 
-i = 0
+# Get country codes
+countries_iso_numeric = pd.read_excel(config.data_dir + '/countries-iso-numeric-m49-conc.xlsx');
+country_iso = countries_iso_numeric['M49 code'].to_list()
 
-for f in files:
+store = []
+for c in countries_iso_numeric.iterrows():
 
-    with open(f, 'r') as file:
-        reader = csv.reader(file, delimiter='\t')
-        df = pd.DataFrame(reader)
+    # Meta
+    iso_code = c[1]['M49 code']
+    iso_acr = c[1]['ISO-alpha3 code']
+    save_dir_c = save_dir + iso_acr + '/'
+    
+    print('Processing: ' + iso_acr + ' (' + str(iso_code) + ')')
 
-    # Fix the header
-    header = df.iloc[0] 
-    df = df[1:] 
-    df.columns = header 
-    df = df.reset_index(drop=True)
+    if os.path.isdir(save_dir_c):
 
-    # Filter
-    df['motCode'] = df['motCode'].astype(int)
-    df = df.loc[df['motCode'] > 0]
+        files = [f for f in glob(save_dir_c + "*.txt")]
+        if len(files) == 0:
+            os.rmdir(save_dir_c)
+        else:
+            
+            # Extract files 
+            assert len(files) == 1
+            
+            for f in files:
 
-    df['cmdCode'] = df['cmdCode'].astype(str)
-    df = df.loc[df['cmdCode'].str.len() == 6]
-        
-    # Append
-    if df.shape[0] > 0:
-        store = pd.concat([store, df], axis=0).reset_index(drop=True)
+                with open(f, 'r') as file:
+                    reader = csv.reader(file, delimiter='\t')
+                    df = pd.DataFrame(reader)
 
-    i = i + 1
-    print(str(i) + '/' + str(len(files)))
+                # Fix the header
+                header = df.iloc[0] 
+                df = df[1:] 
+                df.columns = header 
+                df = df.reset_index(drop=True)
+
+                # Data types
+                df['motCode'] = df['motCode'].astype(int)
+                df['cmdCode'] = df['cmdCode'].astype(str)
+                
+                # Filter
+                if filter_joined_by_transport_mode:
+                    df = df.loc[df['motCode'] > 0]
+
+                if filter_joined_by_hs6:
+                    df = df.loc[df['cmdCode'].str.len() == 6]
+                    
+                # Append
+                if df.shape[0] > 0:
+                    store.extend(df.to_dict(orient='records'))
+
+store = pd.DataFrame(store)
 
 print('Joined store contains ' + '{:,.0f}'.format(store.shape[0]) + ' records.')
 
-store.to_csv(config.work_dir + 'comtrade-joined-records-' + str(config.year) + '.csv')
+store.to_csv(save_dir + 'comtrade-joined-records-' + str(config.year) + '.csv')
 
 print('Finished.')
 
--- pull_comtrade_records.py
+++ pull_comtrade_records.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import comtradeapicall
 from config.config_reader import read_config
@@ -6,6 +7,12 @@ print('Pulling COMTRADE records...')
 
 # Settings
 config = read_config()
+continue_previous_download = config.settings['continue_previous_download']
+
+# Save path
+save_dir = config.work_dir + str(config.year) + '/'
+if not os.path.isdir(save_dir):
+    os.mkdir(save_dir)
 
 # Get country codes
 countries_iso_numeric = pd.read_excel(config.data_dir + '/countries-iso-numeric-m49-conc.xlsx');
@@ -13,21 +20,30 @@ country_iso = countries_iso_numeric['M49 code'].to_list()
 
 for c in countries_iso_numeric.iterrows():
 
+    # Meta
     iso_code = c[1]['M49 code']
     iso_acr = c[1]['ISO-alpha3 code']
-
+    save_dir_c = save_dir + iso_acr + '/'
+    
     print('Processing: ' + iso_acr + ' (' + str(iso_code) + ')')
 
-    comtradeapicall.bulkDownloadFinalFile(
-        config.subscription_key,
-        config.work_dir,
-        typeCode="C",
-        freqCode='A',
-        clCode="HS",
-        period=str(config.year),
-        reporterCode=iso_code,
-        decompress=True,
-    )
+    if not os.path.isdir(save_dir_c):
+
+        os.mkdir(save_dir_c)
+
+        comtradeapicall.bulkDownloadFinalFile(
+            config.subscription_key,
+            save_dir_c,
+            typeCode="C",
+            freqCode='A',
+            clCode="HS",
+            period=str(config.year),
+            reporterCode=iso_code,
+            decompress=True,
+        )
+
+    else:
+        print('Records already exist')
 
     print('.')
         
