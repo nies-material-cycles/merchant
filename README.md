f883ec09380910adf2fbd17167e542cc778f587a
Jacob Fry <30384236+spottedquoll@users.noreply.github.com>
2025-04-24 10:15:04 +0900

    Added switch for legacy result formatting

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Merchant
 Scrapes data from the Comtrade API gently, avoids hitting the 2.5 million row per request limit
 
-## Config
+## Setup
+### Config
 Define a `config.json` file in `/config/` directory.
 
 ```
@@ -11,10 +12,37 @@ Define a `config.json` file in `/config/` directory.
 }
 ```
 
-## Environment
+### Environment
 Requires environment variables:
 ```
 DATA_DIR=/.../
 WORK_DIR=/.../
 ```
 where `WORK_DIR` specifies an output directory and `DATA_DIR` a directory containing the ISO M49 country definitions (countries-iso-numeric-m49-conc.xlsx).
+
+## Legacy format
+mock_legacy_format
+```
+Classification',
+Year',
+'Period',
+'Period Desc.',
+'Aggregate Level', 
+'Is Leaf Code', 
+'Trade Flow Code', 
+'Trade Flow', 
+'Reporter Code'  
+'Reporter'
+'Reporter ISO'
+'Partner Code'
+'Partner'
+'Partner ISO'
+'Commodity Code'
+'Commodity'
+'Qty Unit Code'
+'Qty Unit'
+'Qty'
+'Netweight (kg)'
+'Trade Value (US$)'
+'Flag'
+```
--- join_records.py
+++ join_records.py
@@ -63,6 +63,10 @@ for i, c in enumerate(countries_iso_numeric.iterrows()):
                 if filter_joined_by_hs6:
                     df = df.loc[df['cmdCode'].str.len() == 6]
                     
+                # Country name
+                df['Reporter'] = c[1]['Country or Area']
+
+
                 # Append
                 if df.shape[0] > 0:
                     store.append(df)
@@ -73,7 +77,28 @@ all_store = pd.concat(store, ignore_index=True)
 
 print('Complete store contains ' + '{:,.0f}'.format(all_store.shape[0]) + ' records.')
 
-all_store.to_csv(save_dir + 'comtrade-joined-records-' + str(config.year) + '.csv')
+all_store.to_csv(save_dir + 'comtrade-joined-records-' + str(config.year) + '.csv', index=False)
+
+if config.settings['mock_legacy_format']:
+
+    all_store = all_store.rename(columns={"classificationCode": "Classification",
+                                          "refYear": "Year",
+                                          "period": "Period",
+                                          "reporterCode": "Reporter ISO", 
+                                          "netWgt": "Netweight (kg)"})
+    
+    # Missing columns
+    all_store['Period Desc.'] = all_store['Period']
+    all_store['Aggregate Level'] = 2 
+    all_store['Is Leaf Code'] = 0
+
+    # Set column order
+    col_order = ["Classification", "Year", "Period", 'Period Desc.', 'Aggregate Level', 'Is Leaf Code', 'Trade Flow Code', 'Trade Flow',
+                 "Reporter ISO", 'Netweight (kg)']
+    legacy_store = all_store[col_order]
+
+    # Write to disk
+    legacy_store.to_csv(save_dir + 'comtrade-joined-records-legacy-format-' + str(config.year) + '.csv', index=False)
 
 print('Finished.')
 
